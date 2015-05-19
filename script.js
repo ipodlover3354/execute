56b29e28aa08759dc31564263965d5ca5a8ab5ab
ipodlover3354 <pitts.davis3@hotmail.com>
2015-05-18 20:39:26 -0400

    execute

---
+++ script.js
@@ -0,0 +1,12 @@
+/*Written by ipodlover3354*/
+
+function reloadCommand(msg) {
+	console.log(msg);
+	var x = document.getElementById("x").value;
+	var y = document.getElementById("y").value;
+	var z = document.getElementById("z").value;
+	var cmd = document.getElementById("cmd").value;
+	var e = document.getElementById("e").value;
+	console.log('/execute ' + e + ' ' + x + ' ' + y + ' ' + z + ' ' + cmd);
+	$( "div" ).html('/execute ' + e + ' ' + x + ' ' + y + ' ' + z + ' ' + cmd);
+}
